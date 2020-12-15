93433ecc354f228c2d02f21a1a2220f20b265ad0
Sinan Keskin <sinan@kesk.in>
2020-12-16 01:38:46 +0300

    Remove cached decorator

--- addon/modifiers/litepicker.js
+++ addon/modifiers/litepicker.js
@@ -16,7 +16,6 @@ export default class LitepickerModifier extends Modifier {
     return config['ember-litepicker'] || {};
   }
 
-  @cached
   get _options() {
     const options = this._defaultOptions();
 
