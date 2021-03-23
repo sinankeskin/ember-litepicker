3041c727aea841ebcdd41af7203ca494b7048c93
Sinan Keskin <sinan@kesk.in>
2021-03-23 15:12:36 +0300

    Update calendarSelect test-helper

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -5,14 +5,15 @@ import { assert } from '@ember/debug';
 export async function calendarSelect(selector, selected) {
   assert('`calendarSelect` expect a Date object as second argument', selected);
 
-  let target = find(selector);
+  const target = find(selector);
 
   if (target) {
     await click(target);
   }
 
-  let calendarElement = find('.litepicker');
-  let dayElement = calendarElement.querySelector(
+  const calendarElement = document.querySelector('.litepicker');
+
+  const dayElement = calendarElement.querySelector(
     `.container__main .container__days .day-item[data-time="${selected.getTime()}"]`
   );
 
