8cbb252e60ed6b0e1813e2d11d23a5a843364afc
johanrd <johanroed@gmail.com>
2021-10-19 22:13:20 +0200

    Proposed fix for #321

--- index.js
+++ index.js
@@ -2,19 +2,6 @@
 
 module.exports = {
   name: require('./package').name,
-  options: {
-    autoImport: {
-      alias: {
-        keyboardnav$: 'litepicker/dist/plugins/keyboardnav',
-        mobilefriendly$: 'litepicker/dist/plugins/mobilefriendly',
-        ranges$: 'litepicker/dist/plugins/ranges',
-        multiselect$: 'litepicker/dist/plugins/multiselect',
-      },
-    },
-    babel: {
-      plugins: [require.resolve('ember-auto-import/babel-plugin')],
-    },
-  },
 
   included(app) {
     this._super.included.apply(this, arguments);
